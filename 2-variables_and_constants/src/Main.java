8e05084632e11ac8de19a54f96aac2cb04768870
Eric Kuntz <ematheuskuntz@gmail.com>
2026-04-29 18:27:22 -0300

    started the study

---
+++ 2-variables_and_constants/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        -------------VARIAVEIS----------
+        String txt = "Eric Kuntz";
+
+        System.out.println(txt);
+//            Imprime Eric Kuntz
+        txt = "Eric Matheus Kuntz Thibes";
+
+        System.out.println(txt);
+//            Imprime Eric Matheus Kuntz Thibes
+
+//        txt=12; Se eu tentar atribuir um type Int ocorrerá um problema.
+
+//        ------------CONSTANTES------------------
+        final String txt2 = "Minha primeira constante";
+
+        System.out.println(txt2);
+    }
+}
